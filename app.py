7a823404178674723d44f5a9bef73fb237953a2f
Anirban0011 <bhattandbhatt.999@gmail.com>
2025-11-12 10:21:32 +0530

    upd

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import json
 app = FastAPI(title="cpu-scheduler-app")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:8000", "http://localhost:8000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
